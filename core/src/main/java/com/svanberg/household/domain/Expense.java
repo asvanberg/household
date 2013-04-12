4dfd84f694b4497cf8cc9c37f095eea85bf57975
Andreas Svanberg <andreas.svanberg@mensa.se>
2013-04-12 22:17:21 +0200

    Expense entity

---
+++ core/src/main/java/com/svanberg/household/domain/Expense.java
@@ -0,0 +1,67 @@
+package com.svanberg.household.domain;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * @author Andreas Svanberg (andreass) <andreas.svanberg@mensa.se>
+ * @since 0.1.0
+ */
+@Entity
+public class Expense {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Temporal(TemporalType.DATE)
+    private Date date;
+
+    @Basic
+    private String description;
+
+    @Basic
+    private int cost;
+
+    public Expense() {
+
+    }
+
+    public Expense(Date date, String description, int cost) {
+        this.date = date;
+        this.description = description;
+        this.cost = cost;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+}
