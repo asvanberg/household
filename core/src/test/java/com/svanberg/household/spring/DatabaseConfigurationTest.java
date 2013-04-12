e13ef8cb616192c241c10985f49bf55442911d6d
Andreas Svanberg <andreas.svanberg@mensa.se>
2013-04-12 22:07:15 +0200

    Initial commit

---
+++ core/src/test/java/com/svanberg/household/spring/DatabaseConfigurationTest.java
@@ -0,0 +1,27 @@
+package com.svanberg.household.spring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManagerFactory;
+
+import static org.junit.Assert.assertNotNull;
+
+/**
+ * @author Andreas Svanberg (andreass) <andreas.svanberg@mensa.se>
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = DatabaseConfiguration.class)
+public class DatabaseConfigurationTest {
+
+    @Inject
+    EntityManagerFactory entityManagerFactory;
+
+    @Test
+    public void testConfiguration() throws Exception {
+        assertNotNull("Failed to inject entity manager factory, configuration faulty", entityManagerFactory);
+    }
+}
