739e20da896b82eb905573339904cca284cc4f05
Andreas Svanberg <andreas.svanberg@mensa.se>
2013-04-13 14:53:10 +0200

    Updated web configuration

--- web/src/main/java/com/svanberg/household/web/spring/WebConfiguration.java
+++ web/src/main/java/com/svanberg/household/web/spring/WebConfiguration.java
@@ -2,7 +2,6 @@ package com.svanberg.household.web.spring;
 
 import com.svanberg.household.spring.ContextConfiguration;
 
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
@@ -10,7 +9,6 @@ import org.springframework.context.annotation.Import;
  * @author Andreas Svanberg (andreass) <andreas.svanberg@mensa.se>
  */
 @Configuration
-@ComponentScan("com.svanberg.household.web")
 @Import(ContextConfiguration.class)
 public class WebConfiguration {
 }
