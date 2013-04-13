0cbbaa44afe8034d55b7b8bd524b48054c8476cf
Andreas Svanberg <andreas.svanberg@mensa.se>
2013-04-13 14:51:56 +0200

    Added missing @Service annotation to enable dependency injection

--- core/src/main/java/com/svanberg/household/service/impl/ExpenseServiceImpl.java
+++ core/src/main/java/com/svanberg/household/service/impl/ExpenseServiceImpl.java
@@ -4,12 +4,15 @@ import com.svanberg.household.domain.Expense;
 import com.svanberg.household.repository.ExpenseRepository;
 import com.svanberg.household.service.ExpenseService;
 
+import org.springframework.stereotype.Service;
+
 import javax.inject.Inject;
 import java.util.Date;
 
 /**
  * @author Andreas Svanberg (andreass) <andreas.svanberg@mensa.se>
  */
+@Service
 public class ExpenseServiceImpl implements ExpenseService {
 
     private final ExpenseRepository repository;
