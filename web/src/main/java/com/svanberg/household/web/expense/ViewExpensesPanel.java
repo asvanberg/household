c9d20c561ae075ecb445e4a18ee9d421d9fcbe7c
Andreas Svanberg <andreas.svanberg@mensa.se>
2013-04-14 00:53:49 +0200

    Added wicketeer dependency for typesafe property models. Added table to view expenses panel

--- web/src/main/java/com/svanberg/household/web/expense/ViewExpensesPanel.java
+++ web/src/main/java/com/svanberg/household/web/expense/ViewExpensesPanel.java
@@ -1,6 +1,22 @@
 package com.svanberg.household.web.expense;
 
+import com.svanberg.household.domain.Expense;
+
+import org.apache.wicket.extensions.markup.html.repeater.data.table.DataTable;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.IColumn;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.export.AbstractExportableColumn;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.markup.repeater.data.EmptyDataProvider;
+import org.apache.wicket.markup.repeater.data.IDataProvider;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.ResourceModel;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import static org.wicketeer.modelfactory.ModelFactory.from;
+import static org.wicketeer.modelfactory.ModelFactory.model;
 
 /**
  * @author Andreas Svanberg (andreass) <andreas.svanberg@mensa.se>
@@ -10,5 +26,28 @@ public class ViewExpensesPanel extends Panel {
 
     public ViewExpensesPanel(final String id) {
         super(id);
+
+        add(new DataTable<>(TABLE, getColumns(), getProvider(), getRowsPerPage()));
     }
+
+    private long getRowsPerPage() {
+        return 1;
+    }
+
+    private IDataProvider<Expense> getProvider() {
+        return EmptyDataProvider.getInstance();
+    }
+
+    private List<? extends IColumn<Expense, String>> getColumns() {
+        List<IColumn<Expense, String>> columns = new ArrayList<>();
+        columns.add(new AbstractExportableColumn<Expense, String, Date>(new ResourceModel("date", "Date"), "date") {
+            @Override
+            public IModel<Date> getDataModel(final IModel<Expense> rowModel) {
+                return model(from(rowModel).getDate());
+            }
+        });
+        return columns;
+    }
+
+    static final String TABLE = "table";
 }
--- web/src/test/java/com/svanberg/household/web/expense/ViewExpensesPanelTest.java
+++ web/src/test/java/com/svanberg/household/web/expense/ViewExpensesPanelTest.java
@@ -2,6 +2,7 @@ package com.svanberg.household.web.expense;
 
 import com.svanberg.household.web.test.WicketTest;
 
+import org.apache.wicket.extensions.markup.html.repeater.data.table.DataTable;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -22,5 +23,10 @@ public class ViewExpensesPanelTest extends WicketTest {
     @Test
     public void testRenders() throws Exception {
         assertNotNull("Does not render", panel);
+    }
+
+    @Test
+    public void testHasTable() throws Exception {
+        tester.assertComponent(ViewExpensesPanel.TABLE, DataTable.class);
     }
 }
