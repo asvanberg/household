b08cae2364f3b8772fe303cdfb407bac7a5b18f0
Andreas Svanberg <andreas.svanberg@mensa.se>
2013-04-14 01:57:42 +0200

    Changed to a DefaultDatatable to get headers and pagination and information if there are no expenses.

--- web/src/main/java/com/svanberg/household/web/expense/ViewExpensesPanel.java
+++ web/src/main/java/com/svanberg/household/web/expense/ViewExpensesPanel.java
@@ -4,12 +4,12 @@ import com.svanberg.household.domain.Expense;
 import com.svanberg.household.service.ExpenseService;
 import com.svanberg.household.web.spring.DataProviderPage;
 
-import org.apache.wicket.extensions.markup.html.repeater.data.table.DataTable;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.DefaultDataTable;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.IColumn;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.ISortableDataProvider;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.export.AbstractExportableColumn;
 import org.apache.wicket.extensions.markup.html.repeater.util.SortableDataProvider;
 import org.apache.wicket.markup.html.panel.Panel;
-import org.apache.wicket.markup.repeater.data.IDataProvider;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.model.ResourceModel;
@@ -34,14 +34,14 @@ public class ViewExpensesPanel extends Panel {
     public ViewExpensesPanel(final String id) {
         super(id);
 
-        add(new DataTable<>(TABLE, getColumns(), getProvider(), getRowsPerPage()));
+        add(new DefaultDataTable<>(TABLE, getColumns(), getProvider(), getRowsPerPage()));
     }
 
-    private long getRowsPerPage() {
-        return 1;
+    private int getRowsPerPage() {
+        return 20;
     }
 
-    private IDataProvider<Expense> getProvider() {
+    private ISortableDataProvider<Expense, String> getProvider() {
         return new SortableDataProvider<Expense, String>() {
             @Override
             public Iterator<? extends Expense> iterator(final long first, final long count) {
