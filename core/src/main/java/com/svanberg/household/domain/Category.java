812e63c1d85f8d7e38d26ab603414cf8abfbb8b9
Andreas Svanberg <andreas.svanberg@mensa.se>
2013-04-14 20:41:28 +0200

    Entity for categories

---
+++ core/src/main/java/com/svanberg/household/domain/Category.java
@@ -0,0 +1,48 @@
+package com.svanberg.household.domain;
+
+import javax.persistence.Basic;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+/**
+ * @author Andreas Svanberg (andreass) <andreas.svanberg@mensa.se>
+ */
+@Entity
+public class Category extends DomainObject {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Basic(optional = false)
+    private String name;
+
+    @Basic
+    private String description;
+
+    public Category() {
+    }
+
+    @Override
+    public Serializable getIdentifier() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(final String description) {
+        this.description = description;
+    }
+}
