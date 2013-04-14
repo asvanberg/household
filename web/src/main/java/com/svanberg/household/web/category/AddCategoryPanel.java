86d9cd24ba5f892d2d1758f65af87fb3ab45e433
Andreas Svanberg <andreas.svanberg@mensa.se>
2013-04-14 21:44:39 +0200

    Service method for creating categories

--- web/src/main/java/com/svanberg/household/web/category/AddCategoryPanel.java
+++ web/src/main/java/com/svanberg/household/web/category/AddCategoryPanel.java
@@ -1,9 +1,13 @@
 package com.svanberg.household.web.category;
 
+import com.svanberg.household.service.CategoryService;
+
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.TextArea;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.spring.injection.annot.SpringBean;
 
 /**
  * @author Andreas Svanberg (andreass) <andreas.svanberg@mensa.se>
@@ -11,14 +15,29 @@ import org.apache.wicket.markup.html.panel.Panel;
 public class AddCategoryPanel extends Panel {
     private static final long serialVersionUID = 7362182798476289232L;
 
+    @SpringBean CategoryService categoryService;
+
+    private final TextField<String> name;
+    private final TextArea<String> description;
+
     public AddCategoryPanel(final String id) {
         super(id);
 
-        Form<Void> form = new Form<>(FORM);
+        Form<Void> form = new Form<Void>(FORM) {
+            @Override
+            protected void onSubmit() {
+                categoryService.create(
+                        name.getModelObject(),
+                        description.getModelObject());
+            }
+        };
         add(form);
 
-        form.add(new TextField<>(NAME));
-        form.add(new TextArea<>(DESCRIPTION));
+        name = new TextField<>(NAME, new Model<String>());
+        description = new TextArea<>(DESCRIPTION, new Model<String>());
+
+        form.add(name);
+        form.add(description);
     }
 
     static final String FORM = "form";
--- web/src/test/java/com/svanberg/household/web/category/AddCategoryPanelTest.java
+++ web/src/test/java/com/svanberg/household/web/category/AddCategoryPanelTest.java
@@ -1,19 +1,27 @@
 package com.svanberg.household.web.category;
 
+import com.svanberg.household.service.CategoryService;
 import com.svanberg.household.web.test.WicketTest;
 
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.TextArea;
 import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.util.tester.FormTester;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
 
 import static org.junit.Assert.assertNotNull;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 /**
  * @author Andreas Svanberg (andreass) <andreas.svanberg@mensa.se>
  */
 public class AddCategoryPanelTest extends WicketTest {
+
+    @Mock CategoryService service;
 
     AddCategoryPanel panel;
 
@@ -36,5 +44,21 @@ public class AddCategoryPanelTest extends WicketTest {
     public void testContainsInput() throws Exception {
         tester.assertComponent(path(AddCategoryPanel.FORM, AddCategoryPanel.NAME), TextField.class);
         tester.assertComponent(path(AddCategoryPanel.FORM, AddCategoryPanel.DESCRIPTION), TextArea.class);
+    }
+
+    @Test
+    public void testSubmittingFormCreatesCategory() throws Exception {
+        // given
+        String name = "Name";
+        String description = "Description";
+
+        // when
+        FormTester formTester = tester.newFormTester(AddCategoryPanel.FORM);
+        formTester.setValue(AddCategoryPanel.NAME, name);
+        formTester.setValue(AddCategoryPanel.DESCRIPTION, description);
+        formTester.submit();
+
+        // then
+        verify(service, times(1)).create(eq(name), eq(description));
     }
 }
