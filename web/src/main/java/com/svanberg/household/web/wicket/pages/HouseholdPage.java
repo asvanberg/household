dd1009e7ae25f32a717506ef0aadd82439bf690b
Andreas Svanberg <andreas.svanberg@mensa.se>
2013-04-21 16:33:49 +0200

    Added bootstrap navbar to base page and included bootstrap css files.

--- web/src/main/java/com/svanberg/household/web/wicket/pages/HouseholdPage.java
+++ web/src/main/java/com/svanberg/household/web/wicket/pages/HouseholdPage.java
@@ -1,7 +1,13 @@
 package com.svanberg.household.web.wicket.pages;
 
+import com.svanberg.household.web.expense.ExpensePage;
+
+import de.agilecoders.wicket.markup.html.bootstrap.navbar.ImmutableNavbarComponent;
+import de.agilecoders.wicket.markup.html.bootstrap.navbar.Navbar;
+import de.agilecoders.wicket.markup.html.bootstrap.navbar.NavbarButton;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.model.ResourceModel;
 import org.apache.wicket.model.StringResourceModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 
@@ -15,5 +21,14 @@ public abstract class HouseholdPage extends WebPage {
         super(parameters);
 
         add(new Label("title", new StringResourceModel("page.title", this, getDefaultModel())));
+
+        Navbar navbar = new Navbar("navbar");
+        navbar.fluid();
+        navbar.brandName(new ResourceModel("application.title"));
+        navbar.setPosition(Navbar.Position.TOP);
+        navbar.addComponents(
+                new ImmutableNavbarComponent(new NavbarButton<>(ExpensePage.class, new ResourceModel("page.expense")))
+        );
+        add(navbar);
     }
 }
