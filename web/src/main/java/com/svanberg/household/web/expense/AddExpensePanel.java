a923c5792262a68f7d93c0d13a40ac2374515fc1
Andreas Svanberg <andreas.svanberg@mensa.se>
2013-04-23 22:58:36 +0200

    Changed add expense panel into a modal dialog

--- web/src/main/java/com/svanberg/household/web/expense/AddExpensePanel.java
+++ web/src/main/java/com/svanberg/household/web/expense/AddExpensePanel.java
@@ -6,57 +6,86 @@ import com.svanberg.household.service.CategoryService;
 import com.svanberg.household.service.ExpenseService;
 import com.svanberg.household.web.wicket.EntityModel;
 
+import de.agilecoders.wicket.markup.html.bootstrap.button.ButtonBehavior;
+import de.agilecoders.wicket.markup.html.bootstrap.button.Buttons;
+import de.agilecoders.wicket.markup.html.bootstrap.common.NotificationPanel;
+import de.agilecoders.wicket.markup.html.bootstrap.dialog.Modal;
+import de.agilecoders.wicket.markup.html.bootstrap.form.BootstrapForm;
+import org.apache.wicket.Component;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.form.AjaxFormSubmitBehavior;
 import org.apache.wicket.extensions.markup.html.form.DateTextField;
+import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.*;
-import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
+import org.apache.wicket.model.ResourceModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.apache.wicket.util.time.Duration;
 
 import java.util.Date;
 
 /**
  * @author Andreas Svanberg (andreass) <andreas.svanberg@mensa.se>
  */
-public class AddExpensePanel extends Panel {
+public class AddExpensePanel extends Modal {
     private static final long serialVersionUID = 8253264659998445704L;
 
-    @SpringBean ExpenseService expenseService;
+    @SpringBean private ExpenseService expenseService;
     @SpringBean private CategoryService categoryService;
 
-    private final DateTextField date;
-    private final TextField<Integer> cost;
-    private final TextArea<String> description;
-    private final DropDownChoice<Category> category;
+    private final IModel<Date> date = Model.of(new Date());
+    private final IModel<Integer> cost = new Model<>();
+    private final IModel<String> description = new Model<>();
+    private final IModel<Category> category = new EntityModel<>(Category.class);
+
+    private BootstrapForm<Void> form;
 
     public AddExpensePanel(String id) {
         super(id);
 
-        Form<Void> form = new Form<Void>(FORM) {
-            @Override
-            protected void onSubmit() {
-                Expense expense = expenseService.create(
-                        date.getModelObject(),
-                        description.getModelObject(),
-                        cost.getModelObject());
-                expenseService.setCategory(expense, category.getModelObject());
-            }
-        };
-        add(form);
+        header(new ResourceModel("title"));
+        setFadeIn(false);
 
-        date = new DateTextField(DATE, new Model<Date>()) {
+        addForm();
+
+        addDate();
+        addDescription();
+        addCost();
+        addCategory();
+
+        addFeedback();
+
+        addCreateButton(new ResourceModel("create"));
+        addCloseButton(new ResourceModel("cancel"));
+    }
+
+    private void addFeedback() {
+        NotificationPanel feedback = new NotificationPanel(FEEDBACK);
+        feedback.hideAfter(Duration.seconds(10));
+        form.add(feedback);
+    }
+
+    private void addCreateButton(final IModel<String> label) {
+        Component button = new Label(BUTTON_MARKUP_ID, label);
+        button.add(new ButtonBehavior(Buttons.Type.Primary));
+        button.add(new AjaxFormSubmitBehavior(form, "onclick") {
             @Override
-            protected String getInputType() {
-                return "date";
+            protected void onAfterSubmit(final AjaxRequestTarget target) {
+                appendCloseDialogJavaScript(target);
             }
-        };
-        description = new TextArea<>(DESCRIPTION, new Model<String>());
-        cost = new TextField<Integer>(COST, new Model<Integer>(), Integer.class) {
+
             @Override
-            protected String getInputType() {
-                return "number";
+            protected void onError(final AjaxRequestTarget target) {
+                target.add(getForm());
             }
-        };
-        category = new DropDownChoice<>(CATEGORY, new EntityModel<>(Category.class), categoryService.findAll(), new IChoiceRenderer<Category>() {
+        });
+        addButton(button);
+    }
+
+    private void addCategory() {
+        DropDownChoice<Category> category = new DropDownChoice<>(CATEGORY, this.category, categoryService.findAll(),
+                new IChoiceRenderer<Category>() {
             @Override
             public Object getDisplayValue(final Category object) {
                 return object.getName();
@@ -67,11 +96,51 @@ public class AddExpensePanel extends Panel {
                 return String.valueOf(object.getIdentifier());
             }
         });
-
-        form.add(date);
-        form.add(cost);
-        form.add(description);
+        category.setNullValid(true);
         form.add(category);
+    }
+
+    private void addCost() {
+        TextField<Integer> cost = new TextField<Integer>(COST, this.cost, Integer.class) {
+            @Override
+            protected String getInputType() {
+                return "number";
+            }
+        };
+        cost.setRequired(true);
+        form.add(cost);
+    }
+
+    private void addDescription() {
+        TextArea<String> description = new TextArea<>(DESCRIPTION, this.description);
+        description.setRequired(true);
+        form.add(description);
+    }
+
+    private void addDate() {
+        DateTextField date = new DateTextField(DATE, this.date) {
+            @Override
+            protected String getInputType() {
+                return "date";
+            }
+        };
+        date.setRequired(true);
+        form.add(date);
+    }
+
+    private void addForm() {
+        form = new BootstrapForm<Void>(FORM) {
+            @Override
+            protected void onSubmit() {
+                Expense expense = expenseService.create(
+                        date.getObject(),
+                        description.getObject(),
+                        cost.getObject());
+                expenseService.setCategory(expense, category.getObject());
+            }
+        };
+        form.setOutputMarkupId(true);
+        add(form);
     }
 
     static final String FORM = "form";
@@ -79,4 +148,5 @@ public class AddExpensePanel extends Panel {
     static final String COST = "cost";
     static final String DESCRIPTION = "description";
     static final String CATEGORY = "category";
+    static final String FEEDBACK = "feedback";
 }
