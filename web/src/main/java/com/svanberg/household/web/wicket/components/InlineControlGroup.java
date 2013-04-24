0d99ee7cf08558a16ab516a6801810b3c74b0f18
Andreas Svanberg <andreas.svanberg@mensa.se>
2013-04-24 23:11:17 +0200

    Created an inline version of wicket-bootstrap ControlGroup

---
+++ web/src/main/java/com/svanberg/household/web/wicket/components/InlineControlGroup.java
@@ -0,0 +1,19 @@
+package com.svanberg.household.web.wicket.components;
+
+import de.agilecoders.wicket.markup.html.bootstrap.form.ControlGroup;
+import org.apache.wicket.model.IModel;
+
+/**
+ * @author Andreas Svanberg (andreass) <andreas.svanberg@mensa.se>
+ */
+public class InlineControlGroup extends ControlGroup {
+    private static final long serialVersionUID = 8478883260796237101L;
+
+    public InlineControlGroup(String id, IModel<String> label) {
+        super(id, label);
+    }
+
+    public InlineControlGroup(String id, IModel<String> label, IModel<String> help) {
+        super(id, label, help);
+    }
+}
