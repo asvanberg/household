f250cfd15d7ebe4c065d3568c9f5711f7e314395
Andreas Svanberg <andreas.svanberg@mensa.se>
2013-04-26 00:04:52 +0200

    Added deletion of expenses

--- web/src/main/java/com/svanberg/household/web/expense/ExpensePage.java
+++ web/src/main/java/com/svanberg/household/web/expense/ExpensePage.java
@@ -1,15 +1,26 @@
 package com.svanberg.household.web.expense;
 
+import com.svanberg.household.domain.Expense;
+import com.svanberg.household.service.ExpenseService;
 import com.svanberg.household.web.pages.HouseholdPage;
 
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.AjaxFallbackLink;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+
+import java.util.Collection;
 
 /**
  * @author Andreas Svanberg (andreass) <andreas.svanberg@mensa.se>
  */
 public class ExpensePage extends HouseholdPage {
     private static final long serialVersionUID = -7046290354423141576L;
+
+    private @SpringBean ExpenseService expenseService;
+
+    private final ExpenseTable table;
 
     public ExpensePage(PageParameters parameters) {
         super(parameters);
@@ -19,6 +30,18 @@ public class ExpensePage extends HouseholdPage {
         WebMarkupContainer wmc = new WebMarkupContainer("open");
         dialog.addOpenerAttributesTo(wmc);
         add(wmc);
-        add(new ExpenseTable("list"));
+        table = new ExpenseTable("list");
+        table.setOutputMarkupId(true);
+        add(table);
+
+        AjaxFallbackLink<Void> delete = new AjaxFallbackLink<Void>("delete") {
+            @Override
+            public void onClick(final AjaxRequestTarget target) {
+                Collection<Expense> selection = table.getSelection();
+                expenseService.delete(selection);
+                target.add(table);
+            }
+        };
+        add(delete);
     }
 }
