64875c7cbd2c5856010adc764f26ab73e085f340
Andreas Svanberg <andreas.svanberg@mensa.se>
2013-04-25 23:25:11 +0200

    Created a new expense table and extracted a generic base class for future tables.

---
+++ web/src/main/java/com/svanberg/household/web/expense/ExpenseTable.java
@@ -0,0 +1,68 @@
+package com.svanberg.household.web.expense;
+
+import com.svanberg.household.domain.Expense;
+import com.svanberg.household.service.ExpenseService;
+import com.svanberg.household.web.wicket.components.GenericTable;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.export.AbstractExportableColumn;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.ResourceModel;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Date;
+
+import static org.wicketeer.modelfactory.ModelFactory.from;
+import static org.wicketeer.modelfactory.ModelFactory.model;
+
+/**
+ * @author Andreas Svanberg (andreass) <andreas.svanberg@mensa.se>
+ */
+public class ExpenseTable extends GenericTable<Expense> {
+    private static final long serialVersionUID = 4492167689934515266L;
+
+    private @SpringBean ExpenseService expenseService;
+
+    public ExpenseTable(final String id) {
+        super(id);
+
+        setSelectionColumn(true);
+    }
+
+    @Override
+    protected Iterable<Expense> getEntities(Pageable pageable) {
+        return expenseService.findAll(pageable);
+    }
+
+    @Override
+    protected long count() {
+        return expenseService.count();
+    }
+
+    @Override
+    protected void addColumns() {
+        addColumn(new AbstractExportableColumn<Expense, String, Date>(new ResourceModel("date"), "date") {
+            @Override
+            public IModel<Date> getDataModel(final IModel<Expense> rowModel) {
+                return model(from(rowModel).getDate());
+            }
+        });
+        addColumn(new AbstractExportableColumn<Expense, String, String>(new ResourceModel("description"), "description") {
+            @Override
+            public IModel<String> getDataModel(IModel<Expense> rowModel) {
+                return model(from(rowModel).getDescription());
+            }
+        });
+        addColumn(new AbstractExportableColumn<Expense, String, String>(new ResourceModel("category"), "category") {
+            @Override
+            public IModel<String> getDataModel(IModel<Expense> rowModel) {
+                return model(from(rowModel).getCategoryName());
+            }
+        });
+        addColumn(new AbstractExportableColumn<Expense, String, Integer>(new ResourceModel("cost"), "cost") {
+            @Override
+            public IModel<Integer> getDataModel(IModel<Expense> rowModel) {
+                return model(from(rowModel).getCost());
+            }
+        });
+    }
+}
---
+++ web/src/main/java/com/svanberg/household/web/wicket/components/GenericTable.java
@@ -0,0 +1,165 @@
+package com.svanberg.household.web.wicket.components;
+
+import com.svanberg.household.domain.DomainObject;
+import com.svanberg.household.web.spring.DataProviderPage;
+import com.svanberg.household.web.wicket.EntityModel;
+import de.agilecoders.wicket.markup.html.bootstrap.navigation.BootstrapPagingNavigator;
+import de.agilecoders.wicket.markup.html.bootstrap.navigation.ajax.BootstrapAjaxPagingNavigator;
+import de.agilecoders.wicket.markup.html.bootstrap.table.TableBehavior;
+import org.apache.wicket.extensions.ajax.markup.html.repeater.data.table.AjaxFallbackHeadersToolbar;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.*;
+import org.apache.wicket.extensions.markup.html.repeater.util.SortableDataProvider;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.IModel;
+import org.springframework.data.domain.Pageable;
+
+import java.util.*;
+
+/**
+ * Generic table component for displaying a list of entities.
+ *
+ * @author Andreas Svanberg (andreass) <andreas.svanberg@mensa.se>
+ */
+public abstract class GenericTable<T extends DomainObject> extends Panel {
+    private static final long serialVersionUID = 2853256830432178448L;
+
+    private final List<IColumn<T, String>> columns = new ArrayList<>();
+    private boolean selectionColumn = false;
+    private SelectionColumn<T, String> selectColumn;
+
+    public GenericTable(final String id) {
+        super(id);
+
+        setRenderBodyOnly(true);
+    }
+
+    /**
+     * Returns the entities that should be on the given page.
+     *
+     * @param pageable the page
+     * @return the entities that should be on the page
+     */
+    protected abstract Iterable<T> getEntities(final Pageable pageable);
+
+    /**
+     * Add your columns here.
+     *
+     * @see #addColumn(org.apache.wicket.extensions.markup.html.repeater.data.table.IColumn)
+     */
+    protected abstract void addColumns();
+
+    /**
+     * Returns the total number of entities in this table.
+     *
+     * @return the total number of entities in this table
+     */
+    protected abstract long count();
+
+    /**
+     * Set if a selection column should be included in the table. It will be
+     * the first column.
+     *
+     * @param selectionColumn {@code true} to include the column
+     * @return this
+     */
+    public final GenericTable<T> setSelectionColumn(final boolean selectionColumn) {
+        this.selectionColumn = selectionColumn;
+        return this;
+    }
+
+    /**
+     * Returns whether to add a selection column or not.
+     *
+     * @return {@code true} if a selection column will be added
+     */
+    public final boolean hasSelectionColumn() {
+        return selectionColumn;
+    }
+
+    /**
+     * Returns the currently selected entities.
+     *
+     * @return the currently selected entities
+     */
+    @SuppressWarnings("unchecked")
+    public final Collection<IModel<T>> getSelection() {
+        return selectColumn != null ? selectColumn.getSelection() : Collections.EMPTY_SET;
+    }
+
+    /**
+     * Adds a column to the table.
+     *
+     * @param column column to add
+     * @see #addColumns()
+     */
+    protected final void addColumn(final IColumn<T, String> column) {
+        columns.add(column);
+    }
+
+    /**
+     * Returns the number of rows per page.
+     *
+     * @return the number of rows per page
+     */
+    protected long getRowsPerPage() {
+        return 20;
+    }
+
+    private ISortableDataProvider<T, String> getProvider() {
+        return new TableProvider();
+    }
+
+    @Override
+    protected void onInitialize() {
+        super.onInitialize();
+
+        if (selectionColumn) {
+            addColumn(selectColumn = new SelectionColumn<>());
+        }
+
+        addColumns();
+
+        ISortableDataProvider<T, String> provider = getProvider();
+
+        DataTable<T, String> table = new DataTable<>(TABLE, columns, provider, getRowsPerPage());
+        table.addTopToolbar(new AjaxFallbackHeadersToolbar<>(table, provider));
+        table.addBottomToolbar(new NoRecordsToolbar(table));
+        table.add(new TableBehavior().hover().striped());
+        add(table);
+
+        BootstrapAjaxPagingNavigator paging = new BootstrapAjaxPagingNavigator(PAGING, table) {
+            @Override
+            protected void onConfigure() {
+                super.onConfigure();
+                setVisibilityAllowed(getPageable().getPageCount() > 1L);
+            }
+        };
+        paging.setPosition(BootstrapPagingNavigator.Position.Right);
+        add(paging);
+
+        add(new NavigatorLabel(RESULTS, table));
+    }
+
+    private class TableProvider extends SortableDataProvider<T, String> {
+        private static final long serialVersionUID = 603419089968507278L;
+
+        @Override
+        public Iterator<? extends T> iterator(final long first, final long count) {
+            return getEntities(new DataProviderPage(first, count, getSort())).iterator();
+        }
+
+        @Override
+        public long size() {
+            return count();
+        }
+
+        @Override
+        public IModel<T> model(final T object) {
+            return new EntityModel<>(object);
+        }
+    }
+
+    static final String TABLE = "table";
+    static final String PAGING = "paging";
+    static final String RESULTS = "results";
+}
