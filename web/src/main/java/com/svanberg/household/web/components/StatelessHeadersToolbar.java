1ef3043c00b95958c4904237704543c18e8081e2
Andreas Svanberg <andreas.svanberg@mensa.se>
2013-05-02 02:21:45 +0200

    Added support for styled columns.

--- web/src/main/java/com/svanberg/household/web/components/StatelessHeadersToolbar.java
+++ web/src/main/java/com/svanberg/household/web/components/StatelessHeadersToolbar.java
@@ -1,10 +1,14 @@
 package com.svanberg.household.web.components;
 
+import org.apache.wicket.behavior.AttributeAppender;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.AbstractToolbar;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.DataTable;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.IColumn;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.IStyledColumn;
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
+import org.apache.wicket.model.Model;
 
 /**
  * @author Andreas Svanberg (andreass) <andreas.svanberg@mensa.se>
@@ -17,18 +21,34 @@ public class StatelessHeadersToolbar extends AbstractToolbar
     {
         super(table);
 
-        ListView<? extends IColumn<?,?>> headers = new ListView<IColumn<?, ?>>(HEADERS, table.getColumns())
+        ListView<? extends IColumn<?,?>> headers = new ListView<IColumn<?, ?>>(ROW, table.getColumns())
         {
             @Override
             protected void populateItem(final ListItem<IColumn<?, ?>> item)
             {
-                item.add(item.getModelObject().getHeader(HEADER));
+                populate(item);
             }
         };
 
         add(headers);
     }
 
-    static final String HEADERS = "headers";
+    private void populate(final ListItem<IColumn<?, ?>> item)
+    {
+        IColumn<?, ?> column = item.getModelObject();
+
+        WebMarkupContainer cell = new WebMarkupContainer(CELL);
+
+        if (column instanceof IStyledColumn) {
+            IStyledColumn<?, ?> styledColumn = (IStyledColumn<?, ?>) column;
+            cell.add(new AttributeAppender("class", Model.of(styledColumn.getCssClass()), " "));
+        }
+
+        cell.add(column.getHeader(HEADER));
+        item.add(cell);
+    }
+
+    static final String ROW = "headers";
+    static final String CELL = "cell";
     static final String HEADER = "header";
 }
