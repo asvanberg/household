edb229700ae9474b4cd16393a59a9727c75b814f
Andreas Svanberg <andreas.svanberg@mensa.se>
2013-05-02 18:25:04 +0200

    Stateless headers toolbar with support for sorting.

--- web/src/main/java/com/svanberg/household/web/components/stateless/SortableHeadersToolbar.java
+++ web/src/main/java/com/svanberg/household/web/components/stateless/SortableHeadersToolbar.java
@@ -1,17 +1,119 @@
 package com.svanberg.household.web.components.stateless;
 
+import org.apache.wicket.behavior.AttributeAppender;
+import org.apache.wicket.extensions.markup.html.repeater.data.sort.ISortState;
+import org.apache.wicket.extensions.markup.html.repeater.data.sort.ISortStateLocator;
+import org.apache.wicket.extensions.markup.html.repeater.data.sort.SortOrder;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.AbstractToolbar;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.DataTable;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.IColumn;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.IStyledColumn;
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
+import org.apache.wicket.model.Model;
 
 /**
  * @author Andreas Svanberg (andreass) <andreas.svanberg@mensa.se>
  */
-public class SortableHeadersToolbar extends AbstractToolbar
+public class SortableHeadersToolbar<S> extends AbstractToolbar implements SortableCellLink.ISortingParametersProvider<S>
 {
     private static final long serialVersionUID = 5439576634619819790L;
 
-    public SortableHeadersToolbar(final DataTable<?, ?> table)
+    public static final String SORTING_PAGE_PARAMETER = "sort";
+
+    private final ISortStateLocator<S> sortStateLocator;
+
+    public SortableHeadersToolbar(final DataTable<?, S> table, final ISortStateLocator<S> sortStateLocator)
     {
         super(table);
+        this.sortStateLocator = sortStateLocator;
+
+        add(new ListView<IColumn<?, S>>(ROW, table.getColumns()) {
+            @Override
+            protected void populateItem(final ListItem<IColumn<?, S>> item)
+            {
+                IColumn<?, S> column = item.getModelObject();
+
+                WebMarkupContainer cell;
+                if (column.isSortable())
+                {
+                    cell = newSortableCell(column.getSortProperty(), sortStateLocator.getSortState());
+                }
+                else
+                {
+                    cell = new WebMarkupContainer(CELL);
+                }
+
+                if (column instanceof IStyledColumn)
+                {
+                    IStyledColumn<?, ?> styledColumn = (IStyledColumn<?, ?>) column;
+                    cell.add(new AttributeAppender("class", Model.of(styledColumn.getCssClass()), " "));
+                }
+
+                cell.add(column.getHeader(HEADER).setRenderBodyOnly(true));
+                item.add(cell);
+            }
+        });
     }
+
+    @Override
+    protected void onInitialize()
+    {
+        super.onInitialize();
+
+        String sortingValue = getPage().getPageParameters().get(getSortingParameter()).toOptionalString();
+        if (sortingValue != null)
+        {
+            S sortProperty = getSortingProperty(sortingValue);
+            SortOrder sortOrder = getSortingOrder(sortingValue);
+            sortStateLocator.getSortState().setPropertySortOrder(sortProperty, sortOrder);
+        }
+    }
+
+    @Override
+    public String getSortingParameter()
+    {
+        return SORTING_PAGE_PARAMETER;
+    }
+
+    @Override
+    public Object getSortingValue(final S sortProperty, final SortOrder order)
+    {
+        return (order == SortOrder.DESCENDING ? "!" : "") + sortProperty;
+    }
+
+    @SuppressWarnings("unchecked")
+    protected S getSortingProperty(final String sortingValue)
+    {
+        return (S) (sortingValue.startsWith("!") ? sortingValue.substring(1) : sortingValue);
+    }
+
+    protected SortOrder getSortingOrder(final String sortingValue)
+    {
+        return sortingValue.startsWith("!") ? SortOrder.DESCENDING : SortOrder.ASCENDING;
+    }
+
+    protected SortableCellBorder newSortableCell(final S sortProperty, final ISortState<S> sortState)
+    {
+        return new SortableCellBorder(CELL, sortProperty, nextOrder(sortProperty, sortState), SortableHeadersToolbar.this);
+    }
+
+    protected SortOrder nextOrder(final S sortProperty, final ISortState<S> sortState)
+    {
+        SortOrder order = sortState.getPropertySortOrder(sortProperty);
+
+        if (order == SortOrder.NONE)
+        {
+            return SortOrder.ASCENDING;
+        }
+        else
+        {
+            return order == SortOrder.ASCENDING ? SortOrder.DESCENDING : SortOrder.ASCENDING;
+        }
+    }
+
+    public static final String ROW = "row";
+    public static final String CELL = "cell";
+    public static final String HEADER = "header";
 }
--- web/src/test/java/com/svanberg/household/web/components/stateless/SortableHeadersToolbarTest.java
+++ web/src/test/java/com/svanberg/household/web/components/stateless/SortableHeadersToolbarTest.java
@@ -1,36 +1,100 @@
 package com.svanberg.household.web.components.stateless;
 
 import com.svanberg.household.web.test.WicketTest;
+import org.apache.wicket.extensions.markup.html.repeater.data.sort.ISortStateLocator;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.DataTable;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.IColumn;
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.list.ListView;
+import org.apache.wicket.markup.repeater.data.IDataProvider;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.isA;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * @author Andreas Svanberg (andreass) <andreas.svanberg@mensa.se>
  */
 public class SortableHeadersToolbarTest extends WicketTest
 {
-    @Mock DataTable<String, String> table;
+    @Mock IDataProvider<String> provider;
+    @Mock ISortStateLocator<String> sortStateLocator;
+    @Mock DataTable<String, String> table; // cant actually use the mock since getColumns() is final
+
+    private SortableHeadersToolbar toolbar;
 
     @Before
     public void setUp() throws Exception
     {
-        tester().startComponentInPage(new SortableHeadersToolbar(table));
+        IColumn<String, String> column = mockColumn();
+        startToolbar(Arrays.asList(column));
+    }
+
+    private void startToolbar(final List<IColumn<String, String>> columns)
+    {
+        table = new DataTable<>("id", columns, provider, 10L);
+        toolbar = tester().startComponentInPage(new SortableHeadersToolbar<>(table, sortStateLocator));
     }
 
     @Test
     public void testRenders() throws Exception
     {
-        assertNotNull("Does not render", table);
+        assertNotNull("Does not render", toolbar);
     }
 
     @Test
     public void testStateless() throws Exception
     {
-        assertTrue("Is not stateless", table.isStateless());
+        assertTrue("Is not stateless", toolbar.isStateless());
+    }
+
+    @Test
+    public void testContainsRepeaterForHeaders() throws Exception
+    {
+        tester().assertComponent(path(toolbar, SortableHeadersToolbar.ROW), ListView.class);
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    public void testContainsHeaderForEveryColumn() throws Exception
+    {
+        // given
+        IColumn<String, String> column = mockColumn();
+        List<IColumn<String, String>> columns = Arrays.asList(column, column, column);
+
+        // when
+        startToolbar(columns);
+
+        // then
+        tester().assertComponent(path(toolbar, SortableHeadersToolbar.ROW), ListView.class);
+        ListView<?> component = (ListView<?>) tester().getComponentFromLastRenderedPage(path(toolbar, SortableHeadersToolbar.ROW));
+
+        assertEquals("Wrong number of columns", columns.size(), component.getViewSize());
+    }
+
+    @SuppressWarnings("unchecked")
+    private IColumn<String, String> mockColumn()
+    {
+        IColumn<String, String> column = mock(IColumn.class);
+        when(column.getHeader(isA(String.class))).then(new Answer<Object>()
+        {
+            @Override
+            public Object answer(final InvocationOnMock invocation) throws Throwable
+            {
+                return new WebMarkupContainer((String) invocation.getArguments()[0]);
+            }
+        });
+        return column;
     }
 }
