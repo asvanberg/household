0d9f623030c822f370990f0ee5e33b41291d5f74
Andreas Svanberg <andreas.svanberg@mensa.se>
2013-05-02 16:34:54 +0200

    Added decorating sort link to cell border.

--- web/src/main/java/com/svanberg/household/web/components/stateless/SortableCellBorder.java
+++ web/src/main/java/com/svanberg/household/web/components/stateless/SortableCellBorder.java
@@ -1,5 +1,6 @@
 package com.svanberg.household.web.components.stateless;
 
+import org.apache.wicket.extensions.markup.html.repeater.data.sort.SortOrder;
 import org.apache.wicket.markup.html.border.Border;
 
 /**
@@ -9,8 +10,12 @@ public class SortableCellBorder extends Border
 {
     private static final long serialVersionUID = -3903421327963551000L;
 
-    public SortableCellBorder(final String id)
+    public <S> SortableCellBorder(final String id, S sortProperty, SortOrder sortOrder, SortableCellLink.ISortingParametersProvider<S> sortingParametersProvider)
     {
         super(id);
+
+        addToBorder(new SortableCellLink<>(LINK, sortProperty, sortOrder, sortingParametersProvider));
     }
+
+    public static final String LINK = "link";
 }
--- web/src/test/java/com/svanberg/household/web/components/stateless/SortableCellBorderTest.java
+++ web/src/test/java/com/svanberg/household/web/components/stateless/SortableCellBorderTest.java
@@ -1,24 +1,30 @@
 package com.svanberg.household.web.components.stateless;
 
 import com.svanberg.household.web.test.WicketTest;
+import org.apache.wicket.extensions.markup.html.repeater.data.sort.SortOrder;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
 
 /**
  * @author Andreas Svanberg (andreass) <andreas.svanberg@mensa.se>
  */
 public class SortableCellBorderTest extends WicketTest
 {
+    @Mock SortableCellLink.ISortingParametersProvider<String> sortingParametersProvider;
 
     private SortableCellBorder border;
 
     @Before
     public void setUp() throws Exception
     {
-        border = tester().startComponentInPage(SortableCellBorder.class);
+        when(sortingParametersProvider.getSortingParameter()).thenReturn("sort");
+
+        border = tester().startComponentInPage(new SortableCellBorder("id", "date", SortOrder.ASCENDING, sortingParametersProvider));
     }
 
     @Test
@@ -31,5 +37,11 @@ public class SortableCellBorderTest extends WicketTest
     public void testStateless() throws Exception
     {
         assertTrue("Is not stateless", border.isStateless());
+    }
+
+    @Test
+    public void testContainsSortingLink() throws Exception
+    {
+        tester().assertComponent(path(border, SortableCellBorder.LINK), SortableCellLink.class);
     }
 }
