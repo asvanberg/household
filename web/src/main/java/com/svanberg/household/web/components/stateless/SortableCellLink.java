341d4b5fd555508d347b862fd415f58295084980
Andreas Svanberg <andreas.svanberg@mensa.se>
2013-05-02 16:19:50 +0200

    Test to verify page parameters are updated correctly. Javadoc for the link.

--- web/src/main/java/com/svanberg/household/web/components/stateless/SortableCellLink.java
+++ web/src/main/java/com/svanberg/household/web/components/stateless/SortableCellLink.java
@@ -1,33 +1,107 @@
 package com.svanberg.household.web.components.stateless;
 
+import org.apache.wicket.Page;
+import org.apache.wicket.extensions.markup.html.repeater.data.sort.SortOrder;
 import org.apache.wicket.markup.html.link.StatelessLink;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
 
 /**
+ * A stateless link for sorting. Uses page parameters to specify the sorting.
+ * It is up to the owner of the link to perform the actual sorting from the
+ * available page parameters.
+ *
+ * @param <S> the type of sorting property
+ *
  * @author Andreas Svanberg (andreass) <andreas.svanberg@mensa.se>
  */
-public class SortableCellLink extends StatelessLink<Void>
+public class SortableCellLink<S> extends StatelessLink<Void>
 {
     private static final long serialVersionUID = 1274981167864507968L;
+
+    /**
+     * Interface for providing this link with the proper sorting page parameter
+     * values.
+     *
+     * @param <S> the type of sorting property
+     */
+    public static interface ISortingParametersProvider<S>
+    {
+        /**
+         * The key in the pageparameter to store the sorting value in.
+         *
+         * @return the key for the sorting parameter
+         */
+        String getSortingParameter();
+
+        /**
+         * The value to use as the sorting parameter.
+         *
+         * @param sortProperty the property to sort by
+         * @param order        the order to sort in
+         * @return             the sorting parameter value
+         */
+        Object getSortingValue(S sortProperty, SortOrder order);
+    }
+
+    private final S sortProperty;
+    private final SortOrder order;
+    private final ISortingParametersProvider<S> sortingParametersProvider;
 
     /**
      * Construct.
      *
      * @param id {@inheritDoc}
+     * @param sortProperty              the property this link should sort by
+     * @param order                     the order to sort by
+     * @param sortingParametersProvider the provider for sorting page params
      */
-    public SortableCellLink(final String id)
+    public SortableCellLink(final String id, S sortProperty, SortOrder order, ISortingParametersProvider<S> sortingParametersProvider)
     {
         super(id);
+        this.sortProperty = sortProperty;
+        this.order = order;
+        this.sortingParametersProvider = sortingParametersProvider;
     }
 
+    /**
+     * No-op.
+     */
     @Override
     public void onClick()
     {
         // Noop, sorting done via page-parameters
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected CharSequence getURL()
     {
-        return super.getURL();
+        return urlFor(getPageClass(), createPageParameters());
+    }
+
+    /**
+     * Returns the page that the sorting link points to. Defaults to the
+     * current page.
+     *
+     * @return page the link points to
+     */
+    protected Class<? extends Page> getPageClass()
+    {
+        return getPage().getPageClass();
+    }
+
+    /**
+     * Creates the page parameters to append on this link. Will include all
+     * page parameters already present plus the sorting parameter.
+     *
+     * @return page parameters with sorting added
+     */
+    protected PageParameters createPageParameters()
+    {
+        PageParameters pp = new PageParameters(getPage().getPageParameters());
+        pp.set(sortingParametersProvider.getSortingParameter(), sortingParametersProvider.getSortingValue(sortProperty, order));
+        return pp;
     }
 }
--- web/src/test/java/com/svanberg/household/web/components/stateless/SortableCellLinkTest.java
+++ web/src/test/java/com/svanberg/household/web/components/stateless/SortableCellLinkTest.java
@@ -1,23 +1,36 @@
 package com.svanberg.household.web.components.stateless;
 
 import com.svanberg.household.web.test.WicketTest;
+import org.apache.wicket.extensions.markup.html.repeater.data.sort.SortOrder;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.when;
 
 /**
  * @author Andreas Svanberg (andreass) <andreas.svanberg@mensa.se>
  */
 public class SortableCellLinkTest extends WicketTest
 {
-    private SortableCellLink link;
+    @Mock SortableCellLink.ISortingParametersProvider<String> sortingParametersProvider;
+
+    private SortableCellLink<String> link;
+
+    private String sortProperty = "date";
+    private SortOrder order = SortOrder.ASCENDING;
 
     @Before
     public void setUp()
     {
-        link = tester().startComponentInPage(SortableCellLink.class);
+        when(sortingParametersProvider.getSortingParameter()).thenReturn("sort");
+
+        link = tester().startComponentInPage(new SortableCellLink<>("id", sortProperty, order, sortingParametersProvider));
     }
 
     @Test
@@ -30,5 +43,24 @@ public class SortableCellLinkTest extends WicketTest
     public void testStateless() throws Exception
     {
         assertTrue("Is not stateless", link.isStateless());
+    }
+
+    @Test
+    public void testCreatePageParameters() throws Exception
+    {
+        // given
+        String key = "sort";
+        String value = "value";
+
+        when(sortingParametersProvider.getSortingParameter()).thenReturn(key);
+        when(sortingParametersProvider.getSortingValue(eq(sortProperty), eq(order))).thenReturn(value);
+
+        // when
+
+        // then
+        PageParameters pp = link.createPageParameters();
+
+        assertTrue("Does not contain sort parameter", pp.getNamedKeys().contains(key));
+        assertEquals("Contains wrong sort parameter value", value, pp.get(key).toString());
     }
 }
