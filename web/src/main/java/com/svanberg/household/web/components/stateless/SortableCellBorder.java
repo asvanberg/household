1fd781cb5a3e25fe4d6e5dd4c2ab0e4f2e3018b9
Andreas Svanberg <andreas.svanberg@mensa.se>
2013-05-02 16:37:38 +0200

    Javadoc

--- web/src/main/java/com/svanberg/household/web/components/stateless/SortableCellBorder.java
+++ web/src/main/java/com/svanberg/household/web/components/stateless/SortableCellBorder.java
@@ -4,12 +4,24 @@ import org.apache.wicket.extensions.markup.html.repeater.data.sort.SortOrder;
 import org.apache.wicket.markup.html.border.Border;
 
 /**
+ * A border to be used in table headers to decorate the header with a sorting
+ * link.
+ *
  * @author Andreas Svanberg (andreass) <andreas.svanberg@mensa.se>
  */
 public class SortableCellBorder extends Border
 {
     private static final long serialVersionUID = -3903421327963551000L;
 
+    /**
+     * Construct.
+     *
+     * @param id                        {@inheritDoc}
+     * @param sortProperty              property to sort by
+     * @param sortOrder                 order to sort by
+     * @param sortingParametersProvider provider for sorting page parameters
+     * @param <S>                       type of the sorting parameter
+     */
     public <S> SortableCellBorder(final String id, S sortProperty, SortOrder sortOrder, SortableCellLink.ISortingParametersProvider<S> sortingParametersProvider)
     {
         super(id);
