968c5ab858e7be2ca15e929c4ca4b27c61c91490
Andreas Svanberg <andreas.svanberg@mensa.se>
2013-07-12 07:21:11 +0200

    ref: inline control group now modifies to existing components css classes instead of replacing the entire html

--- web/src/main/java/com/svanberg/household/web/components/InlineControlGroup.java
+++ web/src/main/java/com/svanberg/household/web/components/InlineControlGroup.java
@@ -1,19 +1,31 @@
 package com.svanberg.household.web.components;
 
 import de.agilecoders.wicket.core.markup.html.bootstrap.form.ControlGroup;
+import org.apache.wicket.AttributeModifier;
+import org.apache.wicket.Component;
 import org.apache.wicket.model.IModel;
 
-/**
- * @author Andreas Svanberg (andreass) <andreas.svanberg@mensa.se>
- */
 public class InlineControlGroup extends ControlGroup {
     private static final long serialVersionUID = 8478883260796237101L;
+    private static final AttributeModifier INLINE = new AttributeModifier("class", "help-inline");
 
     public InlineControlGroup(String id, IModel<String> label) {
         super(id, label);
     }
 
-    public InlineControlGroup(String id, IModel<String> label, IModel<String> help) {
-        super(id, label, help);
+    @Override
+    protected Component newFeedbackMessageContainer(String id)
+    {
+        Component component = super.newFeedbackMessageContainer(id);
+        component.add(INLINE);
+        return component;
+    }
+
+    @Override
+    protected Component newHelpLabel(String id, IModel<String> model)
+    {
+        Component component = super.newHelpLabel(id, model);
+        component.add(INLINE);
+        return component;
     }
 }
