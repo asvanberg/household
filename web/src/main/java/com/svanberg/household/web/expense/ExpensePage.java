04bab5fa5aa2e2a6eba479f8314da3c9178699bc
Andreas Svanberg <andreas.svanberg@mensa.se>
2013-07-22 19:59:19 +0200

    ref: removed the train wreck that was GenericTable

--- web/src/main/java/com/svanberg/household/web/expense/ExpensePage.java
+++ web/src/main/java/com/svanberg/household/web/expense/ExpensePage.java
@@ -2,15 +2,29 @@ package com.svanberg.household.web.expense;
 
 import com.svanberg.household.domain.Expense;
 import com.svanberg.household.service.ExpenseService;
+import com.svanberg.household.web.components.DomainProvider;
+import com.svanberg.household.web.components.stateless.SelectColumn;
+import com.svanberg.household.web.components.stateless.SortableHeadersToolbar;
+import com.svanberg.household.web.components.stateless.StatelessPagingNavigator;
 import com.svanberg.household.web.pages.HouseholdPage;
 
 import org.apache.wicket.devutils.stateless.StatelessComponent;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.DataTable;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.IColumn;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.NoRecordsToolbar;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.PropertyColumn;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.StatelessForm;
 import org.apache.wicket.markup.html.form.SubmitLink;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.ResourceModel;
+import org.apache.wicket.request.mapper.parameter.INamedParameters;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.apache.wicket.spring.injection.annot.SpringBean;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author Andreas Svanberg (andreass) <andreas.svanberg@mensa.se>
@@ -19,9 +33,13 @@ import org.apache.wicket.spring.injection.annot.SpringBean;
 public class ExpensePage extends HouseholdPage {
     private static final long serialVersionUID = -7046290354423141576L;
 
+    static final String PAGINATION = "pagination";
+    static final long ROWS_PER_PAGE = 20L;
+
     private @SpringBean ExpenseService expenseService;
 
-    private final ExpenseTable table;
+    private final SelectColumn<Expense,String> selectColumn = new SelectColumn<>();
+    private StatelessForm<Void> form;
 
     public ExpensePage(PageParameters parameters) {
         super(parameters);
@@ -31,15 +49,27 @@ public class ExpensePage extends HouseholdPage {
         WebMarkupContainer wmc = new WebMarkupContainer("open");
         dialog.addOpenerAttributesTo(wmc);
         add(wmc);
-        table = new ExpenseTable("list");
-        table.setOutputMarkupId(true);
-        add(table);
 
-        add(new SubmitLink("delete", table.getForm()) {
+        DataTable<Expense, String> table = createTable(getPageParameters());
+
+        form = new StatelessForm<>("selection");
+        form.add(table);
+
+        add(new StatelessPagingNavigator(PAGINATION, getPageParameters(), table)
+        {
+            @Override
+            protected void onConfigure()
+            {
+                setVisibilityAllowed(getPageable().getPageCount() > 1);
+            }
+        });
+
+        add(new SubmitLink("delete", form)
+        {
             @Override
             public void onSubmit()
             {
-                for (IModel<Expense> model : table.getSelection())
+                for (IModel<Expense> model : selectColumn.getSelection())
                 {
                     expenseService.delete(model.getObject());
                 }
@@ -54,5 +84,31 @@ public class ExpensePage extends HouseholdPage {
         add(new Label("count", count));
         add(new Label("average", count == 0 ? 0 : total / (double) count));
         add(new Label("weekly", expenseService.averageWeeklyExpenses()));
+    }
+
+    @Override
+    protected void onInitialize()
+    {
+        super.onInitialize();
+
+        add(form);
+    }
+
+    private DataTable<Expense, String> createTable(INamedParameters parameters)
+    {
+        List<IColumn<Expense, String>> columns = Arrays.<IColumn<Expense, String>>asList(
+                selectColumn,
+                new PropertyColumn<Expense, String>(new ResourceModel("date"), "date", "date"),
+                new PropertyColumn<Expense, String>(new ResourceModel("description"), "description", "description"),
+                new PropertyColumn<Expense, String>(new ResourceModel("category"), "category", "category"),
+                new PropertyColumn<Expense, String>(new ResourceModel("cost"), "cost", "cost")
+        );
+        DomainProvider<Expense, Long> provider = new DomainProvider<>(expenseService);
+        DataTable<Expense, String> table = new DataTable<>("table", columns, provider, ROWS_PER_PAGE);
+
+        table.addTopToolbar(new SortableHeadersToolbar<>(table, provider, parameters));
+        table.addBottomToolbar(new NoRecordsToolbar(table));
+
+        return table;
     }
 }
--- web/src/test/java/com/svanberg/household/web/expense/ExpensePageTest.java
+++ web/src/test/java/com/svanberg/household/web/expense/ExpensePageTest.java
@@ -1,5 +1,6 @@
 package com.svanberg.household.web.expense;
 
+import com.svanberg.household.domain.Expense;
 import com.svanberg.household.service.CategoryService;
 import com.svanberg.household.service.ExpenseService;
 import com.svanberg.household.web.test.SpringWicketTest;
@@ -7,9 +8,15 @@ import com.svanberg.household.web.test.SpringWicketTest;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
+import org.springframework.data.domain.Pageable;
 
+import java.util.ArrayList;
+
+import static com.svanberg.household.web.expense.ExpensePage.*;
 import static com.svanberg.household.web.test.Assert.assertStateless;
 import static org.junit.Assert.assertNotNull;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
 
 /**
  * @author Andreas Svanberg (andreass) <andreas.svanberg@mensa.se>
@@ -28,13 +35,45 @@ public class ExpensePageTest extends SpringWicketTest
     }
 
     @Test
-    public void testRenders() throws Exception {
+    public void renders() throws Exception {
         assertNotNull("Page does not render", page);
     }
 
     @Test
-    public void testStateless() throws Exception
+    public void stateless() throws Exception
     {
         assertStateless(page);
+    }
+
+    @Test
+    public void pagination_invisible_if_no_expenses() throws Exception
+    {
+        when(expenseService.count()).thenReturn(0L);
+
+        setUp();
+
+        tester().assertInvisible(PAGINATION);
+    }
+
+    @Test
+    public void pagination_invisible_if_only_one_page() throws Exception
+    {
+        when(expenseService.count()).thenReturn(ROWS_PER_PAGE);
+        when(expenseService.list(any(Pageable.class))).thenReturn(new ArrayList<Expense>());
+
+        setUp();
+
+        tester().assertInvisible(PAGINATION);
+    }
+
+    @Test
+    public void pagination_visible_if_more_than_one_page() throws Exception
+    {
+        when(expenseService.count()).thenReturn(ROWS_PER_PAGE + 1);
+        when(expenseService.list(any(Pageable.class))).thenReturn(new ArrayList<Expense>());
+
+        setUp();
+
+        tester().assertVisible(PAGINATION);
     }
 }
