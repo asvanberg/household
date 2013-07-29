6d70fd6aa5eb6ee007e45ead03baca4ecc5f3442
Andreas Svanberg <andreas.svanberg@mensa.se>
2013-07-29 20:30:51 +0200

    add: more tests to verify that paging links actually link to the correct place

--- web/src/test/java/com/svanberg/household/web/components/stateless/StatelessPagingNavigatorTest.java
+++ web/src/test/java/com/svanberg/household/web/components/stateless/StatelessPagingNavigatorTest.java
@@ -1,9 +1,9 @@
 package com.svanberg.household.web.components.stateless;
 
 import com.svanberg.household.web.test.WicketTest;
+import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.navigation.paging.IPageable;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
-import org.apache.wicket.util.string.StringValue;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,9 +13,7 @@ import org.mockito.runners.MockitoJUnitRunner;
 import static com.svanberg.household.web.components.stateless.StatelessPagingNavigator.*;
 import static com.svanberg.household.web.test.Assert.assertStateless;
 import static org.junit.Assert.assertNotNull;
-import static org.mockito.Matchers.eq;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 /**
  * @author Andreas Svanberg (andreass) <andreas.svanberg@mensa.se>
@@ -23,10 +21,9 @@ import static org.mockito.Mockito.when;
 @RunWith(MockitoJUnitRunner.class)
 public class StatelessPagingNavigatorTest extends WicketTest
 {
-    private static final StringValue EMPTY_STRING = StringValue.valueOf((String) null);
+    public static final long CURRENT_PAGE = 2L;
+    public static final long PAGE_COUNT = 4L;
 
-    @Mock
-    private PageParameters parameters;
     @Mock
     private IPageable pageable;
 
@@ -35,10 +32,8 @@ public class StatelessPagingNavigatorTest extends WicketTest
     @Before
     public void setUp()
     {
-        when(pageable.getCurrentPage()).thenReturn(2L);
-        when(pageable.getPageCount()).thenReturn(4L);
-
-        when(parameters.get(eq(PAGING_PAGE_PARAMETER))).thenReturn(EMPTY_STRING);
+        when(pageable.getCurrentPage()).thenReturn(CURRENT_PAGE);
+        when(pageable.getPageCount()).thenReturn(PAGE_COUNT);
     }
 
     @Test
@@ -59,19 +54,75 @@ public class StatelessPagingNavigatorTest extends WicketTest
     public void testPagingParameter() throws Exception
     {
         long page = 1L;
-        when(parameters.get(eq(PAGING_PAGE_PARAMETER))).thenReturn(StringValue.valueOf(page));
 
-        start();
+        PageParameters parameters = new PageParameters();
+        parameters.set(PAGING_PAGE_PARAMETER, page);
+
+        start(parameters);
 
         verify(pageable).setCurrentPage(page);
     }
 
-    private void start()
+    @Test
+    public void first_page_link() throws Exception
     {
-        start(parameters, pageable);
+        start();
+
+        assertLinksToPage(path("first"), 0);
     }
 
-    private void start(PageParameters parameters, IPageable pageable)
+    @Test
+    public void previous_page_link() throws Exception
+    {
+        start();
+
+        assertLinksToPage(path("prev"), CURRENT_PAGE - 1);
+    }
+
+    @Test
+    public void specific_page_link() throws Exception
+    {
+        int page = 1;
+
+        start();
+
+        assertLinksToPage(path("navigation", page, "pageLink"), page);
+    }
+
+    @Test
+    public void next_page_link() throws Exception
+    {
+        start();
+
+        assertLinksToPage(path("next"), CURRENT_PAGE + 1);
+    }
+
+    @Test
+    public void last_page_link() throws Exception
+    {
+        start();
+
+        assertLinksToPage(path("last"), PAGE_COUNT - 1);
+    }
+
+    private void assertLinksToPage(String linkPath, long page)
+    {
+        PageParameters parameters = new PageParameters();
+        parameters.set(PAGING_PAGE_PARAMETER, page);
+
+        @SuppressWarnings("unchecked")
+        Class<? extends WebPage> pageClass =
+                (Class<? extends WebPage>) tester().getLastRenderedPage().getPageClass();
+
+        tester().assertBookmarkablePageLink(path(statelessPagingNavigator, linkPath), pageClass, parameters);
+    }
+
+    private void start()
+    {
+        start(new PageParameters());
+    }
+
+    private void start(PageParameters parameters)
     {
         statelessPagingNavigator = tester().startComponentInPage(new StatelessPagingNavigator("id", parameters, pageable));
     }
