b51cba83396c97905d6caf0bf236b6129aa02c1f
Andreas Svanberg <andreas.svanberg@mensa.se>
2013-07-29 20:36:24 +0200

    Revert "del: unused code"
    This reverts commit 549211918b72090f9d06af51b9137acd9fee5492.

---
+++ web/src/test/java/com/svanberg/household/web/components/stateless/ParameterLinkTest.java
@@ -0,0 +1,36 @@
+package com.svanberg.household.web.components.stateless;
+
+import com.svanberg.household.web.test.WicketTest;
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class ParameterLinkTest extends WicketTest
+{
+    @Test
+    public void testAddsNewParameter() throws Exception
+    {
+        ParameterLink link = new ParameterLink("id", "key", "value", DummyPage.class);
+        tester().startComponentInPage(link);
+
+        PageParameters parameters = tester().getLastRenderedPage().getPageParameters();
+        assertTrue(parameters.get("key").isEmpty());
+
+        tester().clickLink(link);
+
+        parameters = tester().getLastRenderedPage().getPageParameters();
+        assertFalse(parameters.get("key").isEmpty());
+        assertEquals("value", parameters.get("key").toOptionalString());
+    }
+
+    public static class DummyPage extends WebPage
+    {
+        public DummyPage()
+        {
+        }
+    }
+}
