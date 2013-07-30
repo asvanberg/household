653e502528950fe6bcb89feb1b8d53e5e6a49513
Andreas Svanberg <andreas.svanberg@mensa.se>
2013-07-30 04:24:44 +0200

    ref: merged add category panel into the page del: old add category panel

--- web/src/main/java/com/svanberg/household/web/pages/CategoryPage.java
+++ web/src/main/java/com/svanberg/household/web/pages/CategoryPage.java
@@ -1,19 +1,64 @@
 package com.svanberg.household.web.pages;
 
-import com.svanberg.household.web.components.specific.AddCategoryPanel;
+import com.svanberg.household.service.CategoryService;
+import com.svanberg.household.web.components.InlineControlGroup;
+import de.agilecoders.wicket.core.markup.html.bootstrap.form.FormBehavior;
+import de.agilecoders.wicket.core.markup.html.bootstrap.form.FormType;
+import org.apache.wicket.markup.html.form.StatelessForm;
+import org.apache.wicket.markup.html.form.TextArea;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.model.ResourceModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+import javax.inject.Inject;
 
 /**
  * @author Andreas Svanberg (andreass) <andreas.svanberg@mensa.se>
  */
-public class CategoryPage extends HouseholdPage {
-    private static final long serialVersionUID = -3327649337900892864L;
+public class CategoryPage extends HouseholdPage
+{
+    static final String FORM = "form";
+    static final String NAME = "name";
+    static final String DESCRIPTION = "description";
+    static final String INPUT = "input";
 
-    public CategoryPage(PageParameters parameters) {
+    private @Inject CategoryService categoryService;
+
+    private IModel<String> name = new Model<>();
+    private IModel<String> description = new Model<>();
+
+    public CategoryPage(PageParameters parameters)
+    {
         super(parameters);
 
-        add(new AddCategoryPanel(ADD_CATEGORY));
+        add(new AddCategoryForm(CategoryPage.FORM));
+
     }
 
-    static final String ADD_CATEGORY = "add_panel";
+    private class AddCategoryForm extends StatelessForm<Void>
+    {
+        public AddCategoryForm(String id)
+        {
+            super(id);
+
+            add(new FormBehavior(FormType.Horizontal));
+
+            add(new InlineControlGroup(NAME, new ResourceModel("name"))
+                    .add(new TextField<>(INPUT, name)
+                            .setRequired(true)));
+            add(new InlineControlGroup(DESCRIPTION, new ResourceModel("description"))
+                    .add(new TextArea<>(INPUT, description)
+                            .setRequired(true)));
+        }
+
+        @Override
+        protected void onSubmit()
+        {
+            categoryService.create(name.getObject(), description.getObject());
+
+            setResponsePage(CategoryPage.class);
+        }
+    }
 }
--- web/src/test/java/com/svanberg/household/web/pages/CategoryPageTest.java
+++ web/src/test/java/com/svanberg/household/web/pages/CategoryPageTest.java
@@ -2,17 +2,21 @@ package com.svanberg.household.web.pages;
 
 import com.svanberg.household.service.CategoryService;
 import com.svanberg.household.web.test.SpringWicketTest;
+import org.apache.wicket.util.tester.FormTester;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 
-import static org.junit.Assert.assertNotNull;
+import static com.svanberg.household.web.pages.CategoryPage.*;
+import static com.svanberg.household.web.test.Assert.assertStateless;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
 
-/**
- * @author Andreas Svanberg (andreass) <andreas.svanberg@mensa.se>
- */
 public class CategoryPageTest extends SpringWicketTest
 {
+    public static final String NAME_INPUT = path(NAME, NAME + "_body", INPUT);
+    public static final String DESCRIPTION_INPUT = path(DESCRIPTION, DESCRIPTION + "_body", INPUT);
 
     @Mock private CategoryService categoryService;
 
@@ -24,7 +28,53 @@ public class CategoryPageTest extends SpringWicketTest
     }
 
     @Test
-    public void testName() throws Exception {
-        assertNotNull("Page does not render", page);
+    public void renders() throws Exception {
+        tester().assertRenderedPage(CategoryPage.class);
+    }
+
+    @Test
+    public void stateless() throws Exception
+    {
+        assertStateless(page);
+    }
+
+    @Test
+    public void create_category() throws Exception
+    {
+        String name = "Some name";
+        String description = "Some description";
+
+        FormTester formTester = tester().newFormTester(FORM);
+
+        formTester.setValue(NAME_INPUT, name);
+        formTester.setValue(DESCRIPTION_INPUT, description);
+
+        formTester.submit();
+
+        verify(categoryService).create(name, description);
+    }
+
+    @Test
+    public void clears_form_after_creation() throws Exception
+    {
+        FormTester formTester = tester().newFormTester(FORM);
+
+        formTester.setValue(NAME_INPUT, "Some name");
+        formTester.setValue(DESCRIPTION_INPUT, "Some description");
+
+        formTester.submit();
+
+        tester().assertModelValue(path(FORM, NAME_INPUT), null);
+        tester().assertModelValue(path(FORM, DESCRIPTION_INPUT), null);
+    }
+
+    @Test
+    public void name_and_description_required() throws Exception
+    {
+        FormTester formTester = tester().newFormTester(FORM);
+
+        formTester.submit();
+
+        verify(categoryService, never()).create(anyString(), anyString());
     }
 }
